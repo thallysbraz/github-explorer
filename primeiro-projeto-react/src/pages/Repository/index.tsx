c6602db39c1571d84001346ab41b698bff9efa60
thallysbraz <thallysbraz3@gmail.com>
2020-06-22 09:42:21 -0300

    estilizando detalhes

--- primeiro-projeto-react/src/pages/Repository/index.tsx
+++ primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from "react";
-import { useRouteMatch } from "react-router-dom";
+import { useRouteMatch, Link } from "react-router-dom";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+
+import { Header, RepositoryInfo, Issues } from "./styles";
+import logoImg from "../../assets/logo.svg";
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +12,55 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="GitHubExplorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars1.githubusercontent.com/u/39064364?v=4"
+            alt="Avatar"
+          />
+          <div>
+            <strong>thallysbraz/github-explorer</strong>
+            <p>Descrição</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Starts</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="/">
+          <div>
+            <strong>repository.full_name</strong>
+            <p>repository.description</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
